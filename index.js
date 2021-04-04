ff2874606df118fd6f54e19a9316e390da15ffb7
Ruhana006 <ritykarim33616@gmail.com>
2021-04-05 02:28:35 +0600

    order collection added and database posted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require("mongodb").ObjectID;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
@@ -20,24 +21,52 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     console.log("error",err);
   const productCollection = client.db("buyBuyBuyers").collection("products");
+  const orderCollection = client.db("buyBuyBuyers").collection("orders")
   console.log("success");
-
-
-  app.post('/admin',(req,res)=>{
+      
+    app.get('/products',(req , res)=>{
+        productCollection.find()
+        .toArray((err , items) =>{
+            res.send(items)
+            console.log('from database', items);
+        })
+    })
+    app.get('/products/:_id',(req,res)=>{
+        productCollection.find({_id :ObjectId(req.params._id)})
+        .toArray((err, items)=>{
+            res.send(items)
+            console.log('error',err , 'res',items)
+        })
+            
+    })
+  app.post('/admin' , (req , res)=>{
       const newProduct = req.body;
-      console.log("adding",newProduct);
+      console.log('adding product' , newProduct);
       productCollection.insertOne(newProduct)
       .then(result => {
-          console.log('inserted',result.insertedCount)
+          console.log('inserted count' , result.insertedCount)
           res.send(result.insertedCount > 0)
       })
   })
+  app.post('/order', (req , res) => {
+      const newOrder = req.body;
+      orderCollection.insertOne(newOrder)
+      .then(result => {
+          console.log('inserted order', result.insertedCount);
+          res.send(result.insertedCount > 0)
+      })
+  })
+  app.get('/productorders', (req, res) => {
+     orderCollection.find()
+     .toArray((err , items) => {
+         console.log(err);
+         res.send(items)
+         console.log('from order collection', items);
+     })
+  })
+
 //   client.close();
 });
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
